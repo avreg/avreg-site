cfa51cfe32bb55ca06a3c11b5e55a659c7c362fc
vladimir <vladimir.mezin@moonion.com>
2013-08-15 12:34:05 +0300

    timer error fix

--- lib/adb.php
+++ lib/adb.php
@@ -174,11 +174,9 @@ class Adb {
          } else if ((int)$line[7] == 32 ) {
             $line[7] = 'audio';
          }
-
           if($line[8][0]=='-'){
               $line[8] = substr($line[8], 1);
           }
-
          // формирование уникального индекса, для работы кэша в браузере пользователя
         // $events[str_replace(array('/', '.'),'_',$line[5].'_'.$line[2].'_'.$line[0] )] = $line;
          array_push($events, $line);
