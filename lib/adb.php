2bd54d0ad5dc14b14d729283be8eed426becef0a
vladimir <vladimir.mezin@moonion.com>
2013-08-15 12:06:29 +0300

    timer error fix

--- lib/adb.php
+++ lib/adb.php
@@ -177,7 +177,8 @@ class Adb {
 
           $timestamp1 = strtotime($line[0]);
           $timestamp2 = strtotime($line[9]);
-          $line[8] = abs($timestamp1 - $timestamp2);
+          $time_diff= abs($timestamp1 - $timestamp2);
+          $line[8] = time($time_diff);
 
 
          // формирование уникального индекса, для работы кэша в браузере пользователя
